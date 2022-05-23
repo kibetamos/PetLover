f6a7b974324ae231d1ab47b9c6f3735bf729e215
kibet kilel <amos.rotich@strathmore.edu>
2022-05-23 22:36:28 +0300

    Create Routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+// import { BrowserRouter as Router } from react-router;
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navigation />
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+          <HomePage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
